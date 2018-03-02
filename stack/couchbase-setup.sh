79b63bbe3e809efc87bb16fe0eb99e26dc413b9a
Gregory Guillou <gregory.guillou@resetlogs.com>
2018-03-02 04:23:10 +0100

    Add couchbase to docker-compose (#7)

---
+++ stack/couchbase-setup.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+COUCHBASE_BASEURL=${COUCHBASE_BASEURL:-couchbase}
+COUCHBASE_ADMINISTRATOR_USERNAME=${COUCHBASE_ADMINISTRATOR_USERNAME:-admin}
+COUCHBASE_ADMINISTRATOR_PASSWORD=${COUCHBASE_ADMINISTRATOR_PASSWORD:-couchbase}
+COUCHBASE_BUCKET=${COUCHBASE_BUCKET:-bucket}
+COUCHBASE_BUCKET_PASSWORD=${COUCHBASE_BUCKET_PASSWORD:-couchbase}
+
+printf "Kicking off couchbase script...\n"
+for i in {1..60}; do
+  curl -L --fail --silent http://${COUCHBASE_BASEURL}:8091/ >/dev/null 2>&1 && break || true
+  sleep 1
+  printf ".\n"
+done
+sleep 1
+
+printf "Running setup commands...\n"
+
+set -e
+
+curl -X POST --silent http://${COUCHBASE_BASEURL}:8091/pools/default \
+     -d memoryQuota=512 -d indexMemoryQuota=512 \
+     >/dev/null 2>&1
+
+curl --silent http://${COUCHBASE_BASEURL}:8091/node/controller/setupServices \
+     -d services=kv%2cn1ql%2Cindex \
+     >/dev/null 2>&1
+
+curl --silent http://${COUCHBASE_BASEURL}:8091/settings/web -d port=8091 \
+     -d username=$COUCHBASE_ADMINISTRATOR_USERNAME \
+     -d password=$COUCHBASE_ADMINISTRATOR_PASSWORD \
+     >/dev/null 2>&1
+
+curl --silent -u $COUCHBASE_ADMINISTRATOR_USERNAME:$COUCHBASE_ADMINISTRATOR_PASSWORD \
+     -X POST http://${COUCHBASE_BASEURL}:8091/settings/indexes \
+     -d 'storageMode=memory_optimized' \
+     >/dev/null 2>&1
+
+curl --silent -u $COUCHBASE_ADMINISTRATOR_USERNAME:$COUCHBASE_ADMINISTRATOR_PASSWORD \
+     -X POST http://${COUCHBASE_BASEURL}:8091/pools/default/buckets \
+     -d name=$COUCHBASE_BUCKET -d bucketType=couchbase \
+     -d ramQuotaMB=128 -d authType=sasl \
+     -d saslPassword=$COUCHBASE_BUCKET_PASSWORD \
+     >/dev/null 2>&1
+
+printf "Setup script successfully executed... Exiting now\n"
