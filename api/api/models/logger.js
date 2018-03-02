537a35ce35fe28a67efd4210d69cd68fe5d94bc6
gregoryguillou <gregory.guillou@oyst.com>
2018-03-02 02:27:36 +0100

    Lower log-level for npm test

--- api/api/models/logger.js
+++ api/api/models/logger.js
@@ -1,4 +1,7 @@
 const Logger = require('bunyan')
-const log = new Logger({ name: 'lineup', level: 'warn' })
+const log = new Logger({ name: 'lineup', level: 'info' })
+if (process.env.NODE_ENV === 'unit') {
+  log.level('warn')
+}
 
 module.exports = log
