2439ed7d8359346fcfa421dd84ffef38e6f5c5e1
gregoryguillou <gregory.guillou@oyst.com>
2018-03-07 05:28:57 +0100

    Remove license error

--- cli/cmd/workspaceshow.go
+++ cli/cmd/workspaceshow.go
@@ -1,7 +1,3 @@
-// Copyright © 2018 NAME HERE <EMAIL ADDRESS>
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
 package cmd
 
 import (
