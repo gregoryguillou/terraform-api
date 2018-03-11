b390154d6974530d68b70e5195a8775086b60f6e
gregoryguillou <gregory.guillou@missena.com>
2018-03-11 02:22:24 +0100

    Simple couchbase inserts

--- api/api/models/couchbase.js
+++ api/api/models/couchbase.js
@@ -3,6 +3,7 @@ var couchnode = require('couchnode')
 const stream = require('stream')
 const YAML = require('yamljs')
 const couchparam = YAML.load('config/settings.yaml')['couchbase']
+const uuidv4 = require('uuid/v4')
 
 const cluster = new couchbase.Cluster(couchparam['url'])
 cluster.authenticate(couchparam['username'], couchparam['password'])
@@ -32,7 +33,89 @@ class EchoStream extends stream.Writable {
 
 const out = new EchoStream()
 
+function workspacePost (workspace, request, callback) {
+  // TODO: Checks the workspace exists
+  const key = `ws:${workspace['project']}:${workspace['workspace']}`
+  bucket.get(key, function (err, data) {
+    if (err) {
+      callback(err, null)
+    } else if (!data || !data[key]) {
+      let payload = {}
+      payload[key] = {
+        type: 'workspace',
+        project: workspace['project'],
+        workspace: workspace['workspace'],
+        state: 'New',
+        creation: Date.now(),
+        request: {
+          date: Date.now(),
+          action: request['action']
+        },
+        events: [ {event: uuidv4()} ]
+      }
+
+      bucket.insert(payload, (err, cas, existing) => {
+        if (err) {
+          callback(err, null)
+        } else {
+          callback(null, payload)
+        }
+      })
+    } else if (data[key] && data[key]['request'] && data[key]['request']['action']) {
+      callback(null, data)
+      // TODO: manage when there is a need for 409 because there is already a query pending
+    } else {
+      console.log('sectio 4')
+      let payload = data
+      payload[key].request = {
+        date: Date.now(),
+        action: request['action']
+      }
+      payload[key]['events'] = data[key]['events'].unshift(
+        {event: uuidv4()}
+      )
+      bucket.upsert(payload, (err, data) => {
+        if (err) {
+          callback(err, null)
+        } else {
+          callback(null, payload)
+        }
+      })
+    }
+  })
+}
+
+function workspaceDelete (workspace, callback) {
+  const key = `ws:${workspace['project']}:${workspace['workspace']}`
+  bucket.remove(key, (err, cas, misses) => {
+    if (err) {
+      callback(err, null)
+    } else {
+      callback(null, null)
+    }
+  })
+}
+
+function workspaceEndRequest (workspace, callback) {
+  const key = `ws:${workspace['project']}:${workspace['workspace']}`
+  bucket.get(key, function (err, data) {
+    if (err) {
+      callback(err, null)
+    } else if (data && data[key]) {
+      let payload = data
+      delete payload[key].request
+      bucket.upsert(payload, (err, data) => {
+        if (err) callback(err, null)
+        else callback(null, payload)
+      })
+    }
+  })
+}
+
 module.exports = {
   'stdout': out,
-  test: test
+  test: test,
+  workspaceDelete: workspaceDelete,
+  workspaceEndRequest: workspaceEndRequest,
+  workspacePost: workspacePost
 }
--- api/test/api/models/couchbase.js
+++ api/test/api/models/couchbase.js
@@ -1,7 +1,7 @@
 /* eslint-env mocha */
 
 const should = require('should')
-const { test } = require('../../../api/models/couchbase')
+const { test, workspacePost, workspaceDelete, workspaceEndRequest } = require('../../../api/models/couchbase')
 
 describe('models', () => {
   describe('couchbase', function () {
@@ -9,6 +9,38 @@ describe('models', () => {
       test((err, data) => {
         should.not.exist(err)
         should(data).containEql({ testdoc: { name: 'Gregory' } })
+        done()
+      })
+    })
+
+    it('Update an empty workspace with an action', (done) => {
+      workspacePost({project: 'demonstration', workspace: 'qa'}, {action: 'apply'}, (err, data) => {
+        should.not.exist(err)
+        should(data['ws:demonstration:qa']['request']).containEql({ action: 'apply' })
+        done()
+      })
+    })
+
+    it('End current request on workspace', (done) => {
+      workspaceEndRequest({project: 'demonstration', workspace: 'qa'}, (err, data) => {
+        should.not.exist(err)
+        should.not.exist(data['ws:demonstration:qa']['request'])
+        done()
+      })
+    })
+
+    it('Request an action to an existing workspace', (done) => {
+      workspacePost({project: 'demonstration', workspace: 'qa'}, {action: 'apply'}, (err, data) => {
+        should.not.exist(err)
+        console.log(data)
+        should(data['ws:demonstration:qa']['request']).containEql({ action: 'apply' })
+        done()
+      })
+    })
+
+    it('Delete Workspace after it has been used', (done) => {
+      workspaceDelete({project: 'demonstration', workspace: 'qa'}, (err, data) => {
+        should.not.exist(err)
         done()
       })
     })
