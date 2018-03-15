553babd6274d75b881edad711be1ce487247be9b
gregoryguillou <gregory.guillou@missena.com>
2018-03-15 06:14:21 +0100

    Create a simple test for golang

--- cli/cmd/version.go
+++ cli/cmd/version.go
@@ -9,6 +9,42 @@ import (
 	"github.com/spf13/cobra"
 )
 
+func versionService() {
+	cfg, err:=loadConfiguration()
+	if err == nil {
+		token, err:=connectAPI(cfg)
+		if err == nil {
+			client := &http.Client{
+				CheckRedirect: nil,
+			}
+			req, err := http.NewRequest("GET", "http://localhost:10010/version", nil)
+			if err != nil {
+				log.Fatal(err)
+			}
+			var dat map[string]interface{}
+			req.Header.Add("Accept", "application/json")
+			req.Header.Add("Authorization", token)
+			resp, err := client.Do(req)
+			if err != nil {
+				log.Fatal(err)
+			}
+			data, err := ioutil.ReadAll(resp.Body)
+			
+			resp.Body.Close()
+			if err != nil {
+				log.Fatal(err)
+			}
+	
+			if err := json.Unmarshal(data, &dat); err != nil {
+				panic(err)
+			}
+
+			fmt.Println("Client:", version)
+			fmt.Println("Server:", fmt.Sprintf("%s", dat["version"]))
+			
+		}
+	}
+}
 var versionCmd = &cobra.Command{
 	Use:   "version",
 	Short: "Displays lineup versions",
@@ -16,44 +52,7 @@ var versionCmd = &cobra.Command{
 	Displays lineup versions on the client and on the server.
 `,
 	Run: func(cmd *cobra.Command, args []string) {
-		
-		cfg, err:=loadConfiguration()
-  	if err == nil {
-			token, err:=connectAPI(cfg)
-			if err == nil {
-				client := &http.Client{
-					CheckRedirect: nil,
-				}
-				req, err := http.NewRequest("GET", "http://localhost:10010/version", nil)
-				if err != nil {
-					log.Fatal(err)
-				}
-				var dat map[string]interface{}
-				req.Header.Add("Accept", "application/json")
-				req.Header.Add("Authorization", token)
-				resp, err := client.Do(req)
-				if err != nil {
-					log.Fatal(err)
-				}
-				data, err := ioutil.ReadAll(resp.Body)
-				
-				resp.Body.Close()
-				if err != nil {
-					log.Fatal(err)
-				}
-		
-				if err := json.Unmarshal(data, &dat); err != nil {
-					panic(err)
-				}
-
-				fmt.Println("Client:", version)
-				fmt.Println("Server:", fmt.Sprintf("%s", dat["version"]))
-				
-			}
-
-		}
-		
-
+		  versionService()
 	},
 }
 
