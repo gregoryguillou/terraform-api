4be78c4093ca95789c080781bd75dde8f8d9efeb
gregoryguillou <gregory.guillou@missena.com>
2018-03-18 13:06:10 +0100

    setup godep and deploy

--- bin/build-cli.sh
+++ bin/build-cli.sh
@@ -1,5 +1,14 @@
 #!/usr/bin/env bash
 
+curl -L -s https://github.com/golang/dep/releases/download/v0.4.1/dep-linux-amd64 \
+  -o $GOPATH/bin/dep
+
+chmod +x $GOPATH/bin/dep
+
 PROJECT_DIR="$(git rev-parse --show-toplevel)"
-cd cli
+
+cd ${PROJECT_DIR}/cli
+mkdir -p build
+dep ensure
+
 make all
