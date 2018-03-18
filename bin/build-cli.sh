ae14c4349ce704e60794c279ec660771e9968231
Gregory Guillou <gregory.guillou@resetlogs.com>
2018-03-18 13:22:36 +0100

    Add Travis-CI support (#87)

---
+++ bin/build-cli.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -e
+
+curl -L -s https://github.com/golang/dep/releases/download/v0.4.1/dep-linux-amd64 \
+  -o $GOPATH/bin/dep
+
+chmod +x $GOPATH/bin/dep
+
+PROJECT_DIR="$(git rev-parse --show-toplevel)"
+
+cd ${PROJECT_DIR}/cli
+mkdir -p build
+dep ensure
+
+make all
