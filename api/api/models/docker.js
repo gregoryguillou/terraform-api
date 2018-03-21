995fed316cfda6887b7b16a766f2c85449f275cf
gregoryguillou <gregory.guillou@missena.com>
2018-03-21 19:46:23 +0100

    Continue working on the feature

--- api/api/models/docker.js
+++ api/api/models/docker.js
@@ -4,6 +4,8 @@ const YAML = require('yamljs')
 const projects = YAML.load('config/settings.yaml')['projects']
 const { EchoStream } = require('./couchbase')
 const logger = require('./logger')
+const { exec } = require('child_process')
+// const RegExp = require('regexp')
 
 let env = [ 'CONSUL_IP=172.28.0.1' ]
 let createoptions = {}
@@ -18,18 +20,54 @@ function version (name, callback) {
   })
 }
 
-function command (command, config, callback) {
+var workspace = {project: 'demonstration', workspace: 'staging'}
+
+function getenv (command, workspace, callback) {
+  const project = projects.find(p => p.name === workspace.project)
+
+  let populatedEnv = { }
+  let envs = [ ]
+  exec(
+    project.lifecycle.getenv[0].replace(/{{deck\.WORKSPACE}}/, workspace.workspace),
+    {cwd: project.lifecycle.cwd},
+    (err, stdout, stderr) => {
+      if (err) {
+        return err
+      }
+      const val = stdout.split('\n')
+      for (var i in val) {
+        const prop = val[i].split('=')
+        populatedEnv[prop[0]] = prop[1]
+      }
+      project.lifecycle.setenv.forEach(element => {
+        var changedElement = element
+        for (var ev in populatedEnv) {
+          var reg = new RegExp(`{{env.${ev}}}`, 'g')
+          changedElement = changedElement.replace(reg, populatedEnv[`${ev}`])
+        }
+        envs.push(changedElement)
+      })
+      if (project.git && project.git.login) {
+        env.push(`GITHUB_USERNAME=${project.git.login}`)
+        env.push(`GITHUB_PASSWORD=${project.git.password}`)
+      }
+    }
+  )
+  console.log(`docker ${command} ${workspace.workspace} ${JSON.stringify(envs)}`)
+  //callback(command, workspace, envs)
+}
+
+const test = apply(workspace, () => {
+  console.log('This is over!!!')
+})
+
+function command (command, config, env, callback) {
   if (!config || !config.project || !config.workspace || !config.event) {
     return callback(new Error('Cannot run docker; you need the workspace and the event id'), null)
   }
   const stdout = new EchoStream(config.event)
   const project = projects.find(p => p.name === config.project)
-  if (project.git) {
-    env.push(`GITHUB_USERNAME=${project.git.login}`)
-    env.push(`GITHUB_PASSWORD=${project.git.password}`)
-  }
-  project.setenv OUGH!!!!
-  
+
   createoptions = {env}
   let args = ['-c', command, '-w', config.workspace]
   if (command === 'apply' && config.ref) {
@@ -46,7 +84,9 @@ function command (command, config, callback) {
 }
 
 function apply (config, callback) {
-  command('apply', config, callback)
+  getenv('apply', workspace, (command, workspace, envs) => {
+    command('apply', config, callback)
+  })
 }
 
 function destroy (config, callback) {
--- api/projects/demonstration/getenv.sh
+++ api/projects/demonstration/getenv.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+set -e
+
 PROJECT_DIR="$(git rev-parse --show-toplevel)"
 cd ${PROJECT_DIR}/api
 export CONSUL=$(node projects/demonstration/consul.js)
 
-echo "CONSUL=${CONSUL}"
+printf "CONSUL=${CONSUL}"
