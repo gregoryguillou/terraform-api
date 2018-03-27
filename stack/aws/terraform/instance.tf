0d22e8a1a46a576e7493adcba2a787955ca245a9
Gregory Guillou <gregory.guillou@resetlogs.com>
2018-03-27 05:37:35 +0200

    Fix network issue on AWS module (#110)

--- stack/aws/terraform/instance.tf
+++ stack/aws/terraform/instance.tf
@@ -175,6 +175,13 @@ resource "aws_security_group" "deck_security_group" {
   }
 
   ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
     from_port   = 10010
     to_port     = 10010
     protocol    = "tcp"
