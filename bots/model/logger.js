065941b59536e12ba6525afb504a73019cf1a5cc
gregoryguillou <gregory.guillou@missena.com>
2018-04-02 11:29:03 +0200

    Remove bot directory

--- bots/model/logger.js
+++
@@ -1,6 +0,0 @@
-const process = require('process')
-const Logger = require('bunyan')
-const level = process.env.BUNYAN_LOG_LEVEL || 'info'
-const logger = new Logger({ level, name: 'bots' })
-
-module.exports = logger
