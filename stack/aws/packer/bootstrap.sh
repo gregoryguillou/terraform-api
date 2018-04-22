ec5bcb3febe6a7eb695037e61df158db2077b65d
gregoryguillou <gregory.guillou@missena.com>
2018-04-22 08:39:25 +0200

    Add redis stack

--- stack/aws/packer/bootstrap.sh
+++ stack/aws/packer/bootstrap.sh
@@ -14,7 +14,8 @@ mkdir -p /opt/terraform-api && cd /opt/terraform-api
 curl -L https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-Linux-x86_64 \
   -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-mkdir -p /mnt/couchbase/data
+mkdir -p /mnt/data/couchbase
+mkdir -p /mnt/data/redis
 mkdir -p /opt/terraform-api/api
 
 
@@ -28,6 +29,9 @@ curl -LO $REPOSITORY/master/stack/docker/docker-compose.yml
 curl -L $REPOSITORY/master/api/config/settings-template.yaml -o api/settings.yaml
 chmod +x couchbase-setup.sh
 
+# shellcheck disable=SC2016
 sed -i 's/\$PWD.*api.*settings\.yaml\:/\/opt\/terraform-api\/api\/settings.yaml:/' docker-compose.yml
-sed -i '/couchbase-server\"/a\ \ \ \ volumes:\n\ \ \ \ \ \ - \"\/mnt\/couchbase\/data:\/opt\/couchbase\/var\/lib\/couchbase\/data\"' \
+sed -i '/couchbase-server\"/a\ \ \ \ volumes:\n\ \ \ \ \ \ - \"\/mnt\/data\/couchbase:\/opt\/couchbase\/var\/lib\/couchbase\/data\"' \
+  docker-compose.yml
+sed -i '/redis-server\"/a\ \ \ \ volumes:\n\ \ \ \ \ \ - \"\/mnt\/data\/redis:\/data\"' \
   docker-compose.yml
